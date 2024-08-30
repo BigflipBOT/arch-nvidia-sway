8469f08b7bc2a9583f569d5d9c54b35b5a828cd9
BigflipBOT <filip.pyra@gmail.com>
2024-08-30 19:14:53 +0200

    added modules options conf

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 ### initialization
 
 username=$(whoami)
-if [ "$EUID" -eq 0 ]
-  then echo "do not run as root"
+if [ "$EUID" -eq 0 ] ; then 
+  echo "do not run as root (sudo)"
   exit -1
 fi
 
@@ -37,12 +37,17 @@ sudo chmod $(stat -c '%a' /etc/default/grub) tmp
 sudo chown $(stat -c '%u:%g' /etc/default/grub) tmp
 sudo mv tmp /etc/default/grub
 
-# add early loading of nvidia modules, TODO: make sure that is all that is needed
-modprobe nvidia NVreg_OpenRmEnableUnsupportedGpus=1
-modprobe nvidia_drm modeset=1 
+# loading modules to initramfs
+# TODO
+
+# passing module settings conf file for udev
+sudo chown 0:0 ./nvidia_modules.conf
+cp ./nvidia_modules.conf /etc/modprobe.d/
+
+# regenerate initramfs
 sudo mkinitcpio -P
 
-### sway setup
+### wayland setup
 
 # privelage managment
 sudo pacman -S seatd polkit # (technicaly only one is needed but may improve compatibility?)
@@ -59,7 +64,7 @@ sudo sh -c 'echo "MOZ_ENABLE_WAYLAND=1" >> /etc/environment ' # firefox flickeri
 # sudo sh -c 'echo "LIBSEAT_BAVKEND=logind" >> /etc/environment ' # error about seatd at sway startup fix
 sudo sh -c 'echo "_JAVA_AWT_WM_NONREPARENTING=1" >> /etc/environment ' # fix for some java aplications
 
-# copying default config
+# copying sway default config
 mkdir ~/.config/sway
 cp /etc/sway/config ~/.config/sway/config
 
